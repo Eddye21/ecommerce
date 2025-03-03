b974f19ae3eb196e6daef6ce4ac75e322cd23e14
Eduardo Moyla <eduardomoyla@gmail.com>
2025-03-02 20:47:32 -0400

    add cart

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import connectMongoDB from "./db/db.js"
 import { engine } from "express-handlebars"
 import viewsRouter from "./routes/views.routes.js"
 import productRouter from "./routes/product.routes.js"
+import cartRouter from "./routes/cart.routes.js"
 
 dotenv.config()
 
@@ -14,13 +15,14 @@ app.use(express.json())
 //HandleBars
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
-app.set("views", "./src/views");
+app.set("views", "./src/views")
 
 connectMongoDB()
 
 //Endpoits
 app.use("/", viewsRouter)
 app.use("/api/products", productRouter)
+app.use("/api/cart")
 
 app.listen(PORT, () => {
     console.log(`Server online in port : http://localhost:${PORT} `)
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -9,7 +9,6 @@ viewsRouter.get("/", async(req, res) => {
         const limit = 10
         
         const listProducts = await Product.paginate({}, {page, limit, lean: true})
-        console.log(listProducts)
         res.status(200).render("home", listProducts)
     } catch (error) {
         res.status(500).send({status: "Error", message: error.message})
@@ -20,20 +19,9 @@ viewsRouter.get("/:uid", async(req, res) => {
     try {
         const uid = req.params.uid
         const getProductById = await Product.paginate({_id: uid}, {lean: true})
-        console.log(getProductById.docs)
         res.status(200).render("home", getProductById)
     } catch (error) {
         res.status(500).send({status: "Error", message: error.messages})
-    }
-})
-
-viewsRouter.get("/products", async(req, res) => {
-    try {
-        const data = await Product.paginate({})
-        console.log(data.docs)
-        res.render("realTimeProducts", data)
-    } catch (error) {
-        res.send(error.message)
     }
 })
 
