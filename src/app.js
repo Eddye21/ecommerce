19377a7caec7394d499b6f022902a13685d158a3
Eduardo Moyla <eduardomoyla@gmail.com>
2025-03-04 17:17:29 -0400

    Adding socket.io some bugs

--- src/app.js
+++ src/app.js
@@ -5,12 +5,20 @@ import { engine } from "express-handlebars"
 import viewsRouter from "./routes/views.routes.js"
 import productRouter from "./routes/product.routes.js"
 import cartRouter from "./routes/cart.routes.js"
+import {Server} from "socket.io"
+import http from "http"
 
-dotenv.config()
-
+//
 const app = express()
+app.use(express.static("public"))
+
+///
+const server =http.createServer(app)
+const io = new Server(server)
 const PORT = 8081
 app.use(express.json())
+
+dotenv.config()
 
 //HandleBars
 app.engine("handlebars", engine())
@@ -24,6 +32,11 @@ app.use("/", viewsRouter)
 app.use("/api/products", productRouter)
 app.use("/api/cart", cartRouter)
 
-app.listen(PORT, () => {
+//Websocket
+io.on("connection", (socket) => {
+    console.log("Nuevo usuario conectado")
+})
+
+server.listen(PORT, () => {
     console.log(`Server online in port : http://localhost:${PORT} `)
 })
